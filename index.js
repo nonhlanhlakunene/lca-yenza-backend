aeffb8b491acf1fe30d8bed5bf7983edad95e306
AngelaSolomons <angelasolomons03@gmail.com>
2026-09-15 21:14:49 +0200

    Fixed db.js imports

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 import express from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
+import db from './config/db.js'
 
 // Initialize configurations
 dotenv.config();
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3000;
+// const PORT = process.env.PORT || 5000;
 
 // Middleware
 app.use(cors());
