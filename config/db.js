29e4ea59adec2dc96685dee0fb90ee9391c5c93c
Elijah736 <elijahlategan3@gmail.com>
2026-09-15 15:39:00 +0200

    Update db.js

--- config/db.js
+++ config/db.js
@@ -1,1 +1,12 @@
-// This is for the database connection
+const mysql = require("mysql2/promise");
+require("dotenv").config();
+
+const db = mysql.createPool({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    port: process.env.DB_PORT
+});
+
+module.exports = db;// This is for the database connection
