016e0b651b1531fdca6cb9e2698d9756571db836
Angela Solomons <angelasolomons03@gmail.com>
2026-09-16 10:14:59 +0200

    Deleted server.js

--- server.js
+++
@@ -1,9 +0,0 @@
-import express from 'express'
-
-const app = express()
-
-app.use(express.json())
-
-app.get('/', (req, res) => res.send('Yenza Backend is running'))
-
-app.listen(2000, () => console.log('Server running on port 2000'))
